9c0369fc6671b996b7dae4afccfaeb0903a8ed12
Denis Hananein <i@zloylos.me>
2015-08-01 00:07:25 +0300

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+var MongoClient = require('mongodb').MongoClient;
+var App = require('./lib/App'),
+    config = require('./configs/config');
+
+var application = new App({
+    // Setup polling way
+    polling: true
+});
+
+MongoClient.connect(config.MONGO, function(err, db) {
+    if (err) {
+        throw err;
+    }
+
+    console.log("Connected correctly to server");
+
+    application
+        .setDb(db)
+        .getBot()
+        .on('message', function (msg) {
+            application.handle(msg);
+        });
+});
