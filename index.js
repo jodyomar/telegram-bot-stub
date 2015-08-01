0107001dca4ddf5656601da83b8c1b7b66994e73
Denis Hananein <i@zloylos.me>
2015-08-01 13:52:06 +0300

    now work without mongodb

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-var MongoClient = require('mongodb').MongoClient;
-var App = require('./lib/App'),
-    config = require('./configs/config');
-
+var App = require('./lib/App');
 var application = new App({
     // Setup polling way
     polling: true
 });
 
-MongoClient.connect(config.MONGO, function(err, db) {
-    if (err) {
-        throw err;
-    }
-
-    console.log("Connected correctly to server");
-
-    application.setDb(db);
-    application.bot.on('message', function (msg) {
-        application.handle(msg);
-    });
+application.bot.on('message', function (msg) {
+    application.handle(msg);
 });
+
+console.log("App runned");
---
+++ index.mongo.js
@@ -0,0 +1,22 @@
+var MongoClient = require('mongodb').MongoClient;
+var App = require('./lib/App'),
+    config = require('./configs/config');
+
+var application = new App({
+    // Setup polling way
+    polling: true
+});
+
+MongoClient.connect(config.MONGO, function(err, db) {
+    if (err) {
+        throw err;
+    }
+
+    console.log("Connected correctly to MongoDB server");
+    console.log("App runned");
+
+    application.setDb(db);
+    application.bot.on('message', function (msg) {
+        application.handle(msg);
+    });
+});
